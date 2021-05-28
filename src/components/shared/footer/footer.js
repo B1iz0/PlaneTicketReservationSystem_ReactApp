3c50a267553d938e144866b41a5165699b6f15f8
Dmitry Mashkov <b1iz0@mail.ru>
2021-05-28 16:09:26 +0300

    -Deleted useless classes from removed bootstrap

--- src/components/shared/footer/footer.js
+++ src/components/shared/footer/footer.js
@@ -4,7 +4,7 @@ import './footer.css'
 
 const Footer = () => {
     return(
-        <footer className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-dark shadow-sm position-fixed bottom-0 w-100">
+        <footer>
             
         </footer>
     );

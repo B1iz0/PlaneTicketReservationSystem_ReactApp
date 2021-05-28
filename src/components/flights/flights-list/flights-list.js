f29eec8ea93f0b70d456d131deae074f4851f512
Dmitry Mashkov <b1iz0@mail.ru>
2021-05-28 16:50:07 +0300

    -Relocated requests from flights-page to flights-list

--- src/components/flights/flights-list/flights-list.js
+++ src/components/flights/flights-list/flights-list.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FlightsItem from '../flights-list-item';
 import Grid from '@material-ui/core/Grid';
 import Pagination from '@material-ui/lab/Pagination';
 import { makeStyles } from '@material-ui/core/styles';
+import API from '../../../api';
 
 const useStyles = makeStyles((theme) => ({
     flightList: {
@@ -13,14 +14,43 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const FlightsList = ({ flights, flightsAmount }) => {
+const FlightsList = () => {
     const classes = useStyles();
     const flightOnPage = 4;
+    let offset = 0;
     const [ page, setPage ] = useState(1);
+    const [ flights, setFlights ] = useState([]);
+    const [ flightsAmount, setFlightsAmount ] = useState(0);
+
+    useEffect(() => {
+        const loadData = async () => {
+            await API.get(`/flights?offset=${offset}&limit=${flightOnPage}`)
+            .then(response => response.data)
+            .then(data => setFlights(data))
+            .catch(error => {
+                if (error.response) {
+                    console.log(error.response.data);
+                }
+            });
+        }
+        const getFlightsAmount = async () => {
+            await API.get('/flights/count')
+            .then(response => response.data)
+            .then(data => setFlightsAmount(data))
+            .catch(error => {
+                if (error.response) {
+                    console.log(error.response.data);
+                }
+            });
+        }
+
+        getFlightsAmount();
+        loadData();
+    }, []);
 
     const handlePageChange = (event, value) => {
+        offset = (value - 1) * flightOnPage;
         setPage(value);
-        console.log(value);
     }
 
     return flights.length ? (
--- src/components/flights/flights-page/flights-page.js
+++ src/components/flights/flights-page/flights-page.js
@@ -1,40 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import FlightsList from '../flights-list';
-import API from '../../../api';
 
 const FlightsPage = () => {
-    const [ flights, setFlights ] = useState([]);
-    const [ flightsAmount, setFlightsAmount ] = useState(0);
-
-    useEffect(() => {
-        const loadData = async () => {
-            await API.get('/flights')
-            .then(response => response.data)
-            .then(data => setFlights(data))
-            .catch(error => {
-                if (error.response) {
-                    console.log(error.response.data);
-                }
-            });
-        }
-        const getFlightsAmount = async () => {
-            await API.get('/flights/count')
-            .then(response => response.data)
-            .then(data => setFlightsAmount(data))
-            .catch(error => {
-                if (error.response) {
-                    console.log(error.response.data);
-                }
-            });
-        }
-
-        getFlightsAmount();
-        loadData();
-    }, []);
-
     return(
         <>
-            <FlightsList flights={flights} flightsAmount={flightsAmount}/>
+            <FlightsList/>
         </>
     );
 };
